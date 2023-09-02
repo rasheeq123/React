c426a909c7cae8a24fc705267a67b6172982dd04
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-09-02 15:34:57 +0530

    state

--- src/components/EventHandling.jsx
+++ src/components/EventHandling.jsx
@@ -8,7 +8,7 @@ const EventHandling = () => {
         <h4>Click Event</h4>
         <button className="btn btn-primary" onClick={()=>{alert('button was clicked')}} >Click Here</button>  {/* callback function kab or kitti baar cal hoga hme nhi pta */}
         <input type="text" className='form-control
-         mt-4' onChange={(e)=>{console.log (e.target.value)}} />  {/* koi bhi value nikaaal skte h iske through i.e jo bhi user enter kr raha and onnchange mtlb jab koi value change krenge*/}
+         mt-4' onChange={(e)=>{console.log (e.target.value)}} />  {/* koi bhi value nikaaal skte h iske through i.e jo bhi user enter kr raha and onnchange mtlb jab koi value change krenge srf click krne pe nhi ho rhi change, hm enter kr rhe*/}
          {/* <input type="color" className='mt-4'  onChange={(e)=>{console.log(e.target.value);}}/> rating wagera krne ke lie , so that uski value change hoti dikhe */}
         <input type="color" className='mt-4'  onChange={(e)=>{document.body.style.backgroundColor=e.target.value}}/> {/* same background clr usi clr ka hoga jaise jaise change krenge */}
         <input type="range" name='' id='' />
--- src/components/StateManagement.jsx
+++ src/components/StateManagement.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const StateManagement = () => {
+    let count=2;
+
+
+   const [likes, setlikes] = useState(0)
+
   return (
-    <div>StateManagement</div>
+    <div className='container'>
+        <h1 className='text-center'>State Management</h1>
+        <hr />
+        <button className='btn btn-primary' onClick={()=>{count++; console.log(count);}}>Add Count</button>
+
+        <h1>{count}</h1>
+        <button className='btn btn-success mt-4' onClick={()=>{setlikes(likes+1)}}>{likes} Likes</button>
+    </div>
   )
 }
 
