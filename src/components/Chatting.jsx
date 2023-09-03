dda3c80b7c421b6125c87a2f095debd1ce1833f6
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-09-03 22:25:32 +0530

    setTodo

--- src/components/Chatting.jsx
+++ src/components/Chatting.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Chatting = () => {
+const Chatting=()=>{
+    const [todochat, setTodochat] = useState([]);
+     
+    const addNewchat=(e)=>{
+        if(e.code=='Enter'){
+        console.log(e.target.value);
+        setTodochat([...todochat, e.target.value]);
+        console.log([...todochat, e.target.value]);
+    }
+}
+
+
   return (
     <div className="container">
         <h1 className='text-center my-5'> Chat</h1>
             <div className="card">
             <div className="card-header">
+                {todochat.map((todo)=>{return <div className='d-flex justify-content-between mb-4'>
+                    <p>{todo}</p>
+                    <p className="text-primary display-date"><i class="bi bi-check2-all"></i></p>
+                    </div>})}
                 
             </div>
             <div className="card-body"> 
@@ -15,5 +30,7 @@ const Chatting = () => {
     </div>
   )
 }
+
+
 
 export default Chatting
