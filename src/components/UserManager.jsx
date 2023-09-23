ca044a822dea175f45b0b42dcf945a93dc8222e9
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-09-23 14:52:14 +0530

    hook

--- src/components/UserManager.jsx
+++ src/components/UserManager.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const UserManager = () => {
+
+  const getUserData= async ()=>{
+    const res=await fetch('http://localhost:5000/user/getall')
+    console.log(res.status);
+  }
+
+  useEffect(() => {
+    // when component opens
+  
+    return () => {
+
+      //when component closes
+      alert('this component is about to close')
+    }
+  }, [])
+  
+
+
+
   return (
     <div className="vh-100 bg-body-secondary">
         <div className="container py-4">
