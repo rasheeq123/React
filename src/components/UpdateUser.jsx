f0290e2b8839d5a68032f14f69837a890dc312a7
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-09-27 12:13:54 +0530

    return

--- src/components/UpdateUser.jsx
+++ src/components/UpdateUser.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect } from 'react'
+import { Formik } from 'formik'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const UpdateUser = () => {
 
     const {id}= useParams()
+    const [userData, setuserData] = useState(null);
+
+
     const fetchUserData= async()=>{
         const res= await fetch('http://localhost:5000/user/getbyid/'+ id);
         console.log(res.status);
@@ -15,14 +19,53 @@ const UpdateUser = () => {
       fetchUserData()
       
     }, []);
+
+    const submitForm=(values)=>{
+        console.log(values);
+    }
     
 
 
   return (
     <div>
-        <h1>
-            {id}
-        </h1>
+        <div className="col-md-4 mx-auto">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="my-3">User update form</h2>
+
+                        <Formik>
+                            initialValues={userData}
+                            onSubmit={}
+                        </Formik>
+
+
+
+
+                        <form onSubmit={signupform.handleSubmit} >
+
+                        <label>Name</label>
+
+                         <span style={{fontSize: 10, marginLeft:'10px', color: 'red'}} >{signupform.touched.name && signupform.errors.name}</span> {/*touched islie bcoz jab submit kre tbhi wo batae ki short h pehle se type krte saathi naa bataye */}
+                        <input id="name" onChange={signupform.handleChange} value={signupform.values.name} type="text" className='form-control mb-3' />
+
+                        <label>Email</label>
+                        <span style={{fontSize: 10, marginLeft:'10px', color: 'red'}} >{signupform.touched.email && signupform.errors.email}</span>
+                        <input id="email" onChange={signupform.handleChange} value={signupform.values.email} type="text" className='form-control mb-3' />
+
+
+                        <label>Password</label>
+                        <span style={{fontSize: 10, marginLeft:'10px', color: 'red'}} >{signupform.touched.password && signupform.errors.password}</span>
+                        <input id="password" onChange={signupform.handleChange} value={signupform.values.password} type="password" className='form-control mb-3' />
+
+                        <label>Confirm Password</label>
+                        <span style={{fontSize: 10, marginLeft:'10px', color: 'red'}} >{signupform.errors.confirm}</span>
+                        <input id="confirm" onChange={signupform.handleChange} value={signupform.values.confirm} type="password" className='form-control mb-3' />
+
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
     </div>
   )
 }
