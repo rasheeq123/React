21947ab1e648f71f0dde6bd503693409a8df41c0
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-09-27 11:59:30 +0530

    formik

--- src/components/UpdateUser.jsx
+++ src/components/UpdateUser.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 const UpdateUser = () => {
 
     const {id}= useParams()
-;
+    const fetchUserData= async()=>{
+        const res= await fetch('http://localhost:5000/user/getbyid/'+ id);
+        console.log(res.status);
+        const data= await res.json();
+        console.log(data);
+    }
+
+    useEffect(() => { //useEffect by default 2 baar chlta h 
+      fetchUserData()
+      
+    }, []);
+    
+
+
   return (
     <div>
         <h1>
